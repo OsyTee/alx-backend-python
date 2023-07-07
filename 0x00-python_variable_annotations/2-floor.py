32ca80d1160b0c7d704eb49f5320831dc582b3e1
OsyTee <OsyTee@github.com>
2023-07-07 12:54:23 +0000

    task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''Import math module'''
+import math
+
+'''Module  - function to returns floor of a float
+'''
+
+def floor(n: float) -> int:
+    '''returns floor of the given float variable
+    '''
+    return math.floor(n)
