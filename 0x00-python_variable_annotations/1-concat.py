4ced9f309e39ba362d24cd047a543e6fc0a0b1f4
OsyTee <OsyTee@github.com>
2023-07-07 12:31:09 +0000

    Task 1

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # Module 1 - Contains function to concatenate two strings
 
+
 def concat(str1: str, str2: str) -> str:
     # Concatenates the two strings 
     return f"{str1}{str2}"
