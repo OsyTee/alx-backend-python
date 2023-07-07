ea96e207410a0d76794e838951caea07992976b6
OsyTee <OsyTee@github.com>
2023-07-07 13:10:42 +0000

    Task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Function to returns return string representation of a float
+'''
+
+
+def to_str(n: float) -> str:
+    '''returns string representation on "n" float
+    '''
+    return str(n)
