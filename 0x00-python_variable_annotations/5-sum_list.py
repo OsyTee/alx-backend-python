2a715fa1ed9496939f5e633ffa5b47b266499413
OsyTee <OsyTee@github.com>
2023-07-07 13:49:05 +0000

    task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Function that takes an input list of floats and returns their sum as float.
+'''
+
+
+def sum_list(input_list: float) -> float:
+    ''' Calculates and returns the sum of all floats in the list
+    '''
+    return sum(input_list)
